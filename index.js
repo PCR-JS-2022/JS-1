c1edd94d9dad14105fc99cd6381bf5d6ff6a61f7
nvkvasiliy <vases74@gmail.com>
2022-01-17 20:31:08 +0500

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+/**
+ * @param {string} date - дата отсчета
+ * @param {
+ *  Array<{
+ *    name: string,
+ *    birthdate: string,
+ *  }>
+ * } phoneList - список друзей из телефонной книги
+ * @returns {
+ *  Array<{
+ *    name: string,
+ *    birthdate: string,
+ *  }>
+ * }
+ */
+export function getNextBirthdays(date, phoneList) {
+
+}
+
+/**
+ * @param {
+ *  Array<{
+ *    name: string,
+ *    birthdate: string,
+ *  }>
+ * } phoneList - список друзей из телефонной книги
+ * @returns {
+ *  Array<{
+ *    month: string,
+ *    friends: Array<{
+ *      name: string,
+ *      birthdate: string,
+ *    }>
+ *  }>
+ * }
+ */
+export function getMonthsList(phoneList) {
+
+}
+
+/**
+ * @param {
+ *  Array<{
+ *    name: string,
+ *    birthdate: string,
+ *    wishList: Array<{
+ *      title: string,
+ *      price: number
+ *    }>
+ *  }>
+ * } phoneList - список друзей из телефонной книги
+ * @returns {
+ *  {
+ *    friendsList: Array<{
+ *      name: string,
+ *      birthdate: string,
+ *      present: {
+ *        title: string,
+ *        price: number,
+ *      },
+ *    }>,
+ *    totalPrice: number
+ *  }>
+ * }}
+ */
+export function getMinimumPresentsPrice(phoneList) {
+
+}
