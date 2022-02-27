cb2a87e65af63cc10d5bb96578254da047e53e68
murrnyaka <murrrnyaka@mail.ru>
2022-02-27 19:16:11 +0500

    Первая задача

--- index.js
+++ index.js
@@ -17,9 +17,49 @@
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
  * @returns {Array<Person>} массив друзей, у которых дни рождения после даты отсчета
  */
- export function getNextBirthdays(date, phoneList) {
-
+ const phoneList2 = [
+    {
+      name: "Александра",
+      birthdate: "21.05.2001",
+    },
+    {
+      name: "Егор",
+      birthdate: "06.08.1976",
+    },
+    {
+      name: "Роман",
+      birthdate: "14.04.2000",
+    },
+    {
+      name: "Василий",
+      birthdate: "27.02.1980",
+    },
+  ];
+  
+function getNextBirthdays(date, phoneList) {
+    const reg = /^\d{2}\.\d{2}\.\d{4}$/;
+    if(!Array.isArray(phoneList) || !reg.test(date)) return []; 
+    date = date.split("."); 
+    let result = []; 
+    for(let i = 0; i < phoneList.length; i++) {
+      let currentDate = phoneList[i].birthdate.split("."); 
+      if(+currentDate[2] > +date[2]) continue; 
+      if(+currentDate[1] > +date[1]) { 
+        result.push(phoneList[i]);
+      } 
+      if(+currentDate[1] === +date[1] && +currentDate[0] >= +date[0]) { 
+        result.push(phoneList[i]);
+      }
+    }
+    return result.sort((a, b) => {
+      a = a.birthdate.split(".").reverse().join("-");
+      b = b.birthdate.split(".").reverse().join("-");
+      return Date.parse(a) - Date.parse(b);
+    });
 };
+
+console.log(getNextBirthdays('21.01.2002', phoneList2));
+  
 
 /**
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
@@ -28,7 +68,7 @@
  *    friends: Array<Person>,
  *  }>}
  */
-export function getMonthsList(phoneList) {
+function getMonthsList(phoneList) {
 
 };
 
@@ -47,7 +87,7 @@ export function getMonthsList(phoneList) {
  *    totalPrice: number
  *  }}
  */
-export function getMinimumPresentsPrice(phoneList) {
+function getMinimumPresentsPrice(phoneList) {
 
 };
 
