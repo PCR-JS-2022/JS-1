4f4bf5d30f0934b9a608900e616fd6d177ed01ce
Simivb <mishurin.ivan@gmail.com>
2022-03-02 03:19:53 +0500

    Первая попытка решения

--- index.js
+++ index.js
@@ -17,8 +17,19 @@
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
  * @returns {Array<Person>} массив друзей, у которых дни рождения после даты отсчета
  */
- export function getNextBirthdays(date, phoneList) {
-
+export function getNextBirthdays(date, phoneList) {
+    let today = date.split('.');
+    nextBirthdays = []
+    if (today[0] < 1 || today[0] > 31 || today[1] < 1 || today[1] > 12 || today[2] < 1000 || today[2] > 9999 || !(Array.isArray(phoneList)))
+        return []
+    else phoneList.forEach(element => {
+        let listDate = element.birthdate.split('.')
+        if ((listDate[2] <= today[2] && listDate[1] > today[1]) ||
+            (listDate[2] <= today[2] && listDate[1] == today[1] && listDate[0] > today[0]))
+            nextBirthdays.push(element)
+    });
+    nextBirthdays.sort((a, b) => a.birthdate.split('.')[1] > b.birthdate.split('.')[1] ? 1 : -1)
+    return console.log(nextBirthdays)
 };
 
 /**
@@ -30,6 +41,46 @@
  */
 export function getMonthsList(phoneList) {
 
+    if (!(Array.isArray(phoneList))) {
+        return []
+    }
+
+    const monthName = {
+        01: 'январь',
+        02: 'февраль',
+        03: 'март',
+        04: 'апрель',
+        05: 'май',
+        06: 'июнь',
+        07: 'июль',
+        08: 'август',
+        09: 'сентябрь',
+        10: 'октябрь',
+        11: 'ноябрь',
+        12: 'декабрь'
+    };
+
+    let monthsList = []
+    phoneList.sort((a, b) => a.birthdate.split('.')[1] > b.birthdate.split('.')[1] ? 1 : -1)
+    phoneList.forEach(element => {
+        let month = element.birthdate.split('.')[1]
+        let flag = false
+        for (let m of monthsList) {
+            if (m.month == monthName[parseInt(month)]) {
+                m.friends.push(element);
+                flag = true
+                break;
+            }
+
+        }
+        if (!flag) {
+            monthsList.push({
+                month: monthName[parseInt(month)],
+                friends: [element]
+            })
+        }
+    })
+    return monthsList
 };
 
 /**
