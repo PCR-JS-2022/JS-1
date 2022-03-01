bbe316fcd0740aa9043acc9d59c893f3c227f0ea
Svetomuzika <sozonov_artem@mail.ru>
2022-03-01 14:30:49 +0500

    Задание 1 done (Так и не понял, что было не так)

--- index.js
+++ index.js
@@ -54,9 +54,11 @@ function getDate(date){
 }
 
 function checkDate(date){
-  let newdate = date.split('.');
-  return (newdate.length === 3 && newdate[0].length === 2 &&
-    newdate[1].length === 2 && newdate[2].length === 4);
+  if(typeof date !== 'string')
+    return false;
+  let = date.split('.');
+  return (date.length === 3 && date[0].length === 2 &&
+    date[1].length === 2 && date[2].length === 4);
 }
 
 const phoneList = [
