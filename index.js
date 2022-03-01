aa83f3e9d5832e08396cd65d8472d5ed5a52d585
Svetomuzika <sozonov_artem@mail.ru>
2022-03-01 13:20:19 +0500

    Задание 1 (Попытка 1.3)

--- index.js
+++ index.js
@@ -22,7 +22,7 @@
 
  function getNextBirthdays(date, phoneList) {
   date = date.split('.');
-  if(!Array.isArray(phoneList) || checkDate(date) || phoneList.length === 0) return [];
+  if(Array.isArray(phoneList) || checkDate(date) || phoneList.length === 0) return [];
   let dateYear = +date[2];
   let dateMonth = +date[1];
   let dateDay = +date[0];
