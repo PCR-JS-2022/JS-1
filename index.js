5f021752718c5c40dfb6c5077e932c337330b36e
Svetomuzika <sozonov_artem@mail.ru>
2022-03-01 14:01:25 +0500

    Задание 1 (Попытка 2.0)

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ function getDate(date){
 }
 
 function checkDate(date){
+  if(typeof date !== 'string')
+    return false;
   return (date.length === 3 && date[0].length === 2 &&
     date[1].length === 2 && date[2].length === 4);
 }
