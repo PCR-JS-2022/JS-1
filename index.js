93de3f29c3cc47438ac2fb1fccf42fe51e1a985d
LenaVas <vasyasina.lena11@gmail.com>
2022-03-01 22:39:01 +0500

    Сделала первую задачу

--- index.js
+++ index.js
@@ -12,14 +12,35 @@
  * @property {number} price - стоимость подарка
  */
 
+function toDate(date) {
+ const data = date.split(".");
+ return new Date(parseInt(data[2]), parseInt(data[1]), parseInt(data[0]));
+}
+
+function getSortFunction(date1, date2) {
+ return toDate(date1).setFullYear(0) - toDate(date2).setFullYear(0);
+}
+
 /**
  * @param {string} date - дата отсчета
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
  * @returns {Array<Person>} массив друзей, у которых дни рождения после даты отсчета
  */
- export function getNextBirthdays(date, phoneList) {
+function getNextBirthdays(date, phoneList) {
+ if (Array.isArray(phoneList) && typeof date === "string" && /^\d{2}.\d{2}.\d{4}$/.test(date)) {
+  return [];
+ }
 
-};
+ const currentDate = toDate(date);
+ return phoneList
+     .filter((person) => {
+      const birthDate = toDate(person.birthdate);
+      if (birthDate > currentDate) return false;
+      if (birthDate.getMonth() > currentDate.getMonth()) return true;
+      return ((birthDate.getMonth() === currentDate.getMonth() && birthDate.getDay() >= currentDate.getDay()));
+     })
+     .sort((person1, person2) => getSortFunction(person1.birthdate, person2.birthdate));
+}
 
 /**
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
@@ -28,7 +49,7 @@
  *    friends: Array<Person>,
  *  }>}
  */
-export function getMonthsList(phoneList) {
+function getMonthsList(phoneList) {
 
 };
 
@@ -47,7 +68,7 @@ export function getMonthsList(phoneList) {
  *    totalPrice: number
  *  }}
  */
-export function getMinimumPresentsPrice(phoneList) {
+function getMinimumPresentsPrice(phoneList) {
 
 };
 
