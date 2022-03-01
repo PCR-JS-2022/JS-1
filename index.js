649c9a875b995b0447a7146f4a837c2e701375cb
equsix <Omar.m.zaid@hotmail.com>
2022-03-01 23:23:25 +0500

    1 попытка

--- index.js
+++ index.js
@@ -13,13 +13,27 @@
  */
 
 /**
+ * @typedef SplitDate
+ * @type {object}
+ * @property {number} day - день
+ * @property {number} month - месяц
+ * @property {number} year - год
+ */
+
+/**
  * @param {string} date - дата отсчета
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
  * @returns {Array<Person>} массив друзей, у которых дни рождения после даты отсчета
  */
- export function getNextBirthdays(date, phoneList) {
+function getNextBirthdays(date, phoneList) {
+ const normedDate = getDate(date)
+ if (!Array.isArray(phoneList) || !normedDate)
+  return []
 
-};
+ const friendsList = phoneList.filter(person => checkDate(getDate(person.birthdate), normedDate))
+
+ return friendsList.sort((a, b) => sortByBirthday(getDate(a.birthdate), getDate(b.birthdate)))
+}
 
 /**
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
@@ -28,9 +42,24 @@
  *    friends: Array<Person>,
  *  }>}
  */
-export function getMonthsList(phoneList) {
+function getMonthsList(phoneList) {
+ if (!Array.isArray(phoneList)) return []
+ const tempPhoneList = phoneList.sort((a, b) => sortByBirthday(getDate(a.birthdate), getDate(b.birthdate)))
+ let monthsList = []
 
-};
+ for (const person of tempPhoneList) {
+  const birthdate = getDate(person.birthdate)
+  const month = birthdate.toLocaleString('ru-RU', {month: 'long'})
+  const monthItem = monthsList.find(item => item.month === month)
+
+  if (monthItem) {
+   monthItem.friends.push(person)
+  } else {
+   monthsList.push({month, friends: [person]})
+  }
+ }
+ return monthsList
+}
 
 /**
  * @param {Array<{
@@ -38,17 +67,88 @@ export function getMonthsList(phoneList) {
  *    birthdate: string,
  *    wishList: Array<Gift>
  *  }>} phoneList - список друзей из телефонной книги
- * @returns {{
- *    friendsList: Array<{
- *      name: string,
- *      birthdate: string,
- *      present: Gift
- *    }>,
- *    totalPrice: number
- *  }}
+ * @returns {{totalPrice: number, friendsList: *[]}}
  */
-export function getMinimumPresentsPrice(phoneList) {
+function getMinimumPresentsPrice(phoneList) {
+ if (!Array.isArray(phoneList)) return []
 
-};
+ return phoneList.reduce((result, person) => {
+  if (person.wishList) {
+   person.wishList.sort((a, b) => a.price - b.price)
+   result.totalPrice += person.wishList[0].price
+   result.friendsList.push({name: person.name, birthdate: person.birthdate, present: person.wishList[0]})
+  } else {
+   result.friendsList.push({ ...person, present: undefined })
+  }
+  return result
+ }, { totalPrice: 0, friendsList: [] })
+}
 
-module.exports = { getNextBirthdays, getMonthsList, getMinimumPresentsPrice };
+/**
+ * @param {string} date - дата
+ * @returns {SplitDate | undefined}
+ */
+function getSplitDate(date) {
+ if (typeof date !== 'string')
+  return undefined
+
+ const sd = date.split('.')
+ if (sd.length !== 3)
+  return undefined
+
+ const [day, month, year] = sd
+
+ if (day.length !== 2 || month.length !== 2 || year.length !== 4)
+  return undefined
+
+ return {day: +day, month: +month, year: +year}
+}
+
+/**
+ * @param {string} date - дата
+ * @returns {Date | undefined}
+ */
+function getDate(date) {
+ const sd = getSplitDate(date)
+ if (!sd) return undefined
+
+ const {day, month, year} = sd
+
+ return new Date(year, month - 1, day)
+}
+
+/**
+ * @param {Date} date - дата для проверки
+ * @param {Date} currentDate - текущая переданная дата
+ * @returns {boolean}
+ */
+function checkDate(date, currentDate) {
+ if (date.getFullYear() < currentDate.getFullYear()) {
+  if (currentDate.getMonth() === date.getMonth()) {
+   return currentDate.getDate() <= date.getDate()
+  }
+  return currentDate.getMonth() < date.getMonth()
+ }
+ return date.getFullYear() === currentDate.getFullYear()
+     && currentDate.getMonth() === date.getMonth()
+     && currentDate.getDate() === date.getDate()
+}
+
+/**
+ * @param {Date} a - первая дата
+ * @param {Date} b - вторая дата
+ * @returns {number}
+ */
+function sortByBirthday(a, b) {
+ if (a.getMonth() > b.getMonth()) {
+  return 1
+ } else if (a.getMonth() === b.getMonth()) {
+  if (a.getDate() === b.getDate()) {
+   return a.getFullYear() - b.getFullYear()
+  }
+  return a.getDate() - b.getDate()
+ }
+ return -1
+}
+
+module.exports = {getNextBirthdays, getMonthsList, getMinimumPresentsPrice};
