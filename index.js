f5ed7995d0df6a671a865512a9ce2fddaa70c7b4
Svetomuzika <sozonov_artem@mail.ru>
2022-03-01 12:11:10 +0500

    Task 1

--- index.js
+++ index.js
@@ -17,9 +17,71 @@
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
  * @returns {Array<Person>} массив друзей, у которых дни рождения после даты отсчета
  */
- export function getNextBirthdays(date, phoneList) {
 
+
+
+ function getNextBirthdays(date, phoneList) {
+  date = date.split('.');
+  // let firstDate = new Date();
+  // firstDate.setFullYear(date[0],(date[1] - 1 ),date[2]);
+  // console.log(firstDate);
+
+  let arr = [];
+
+
+  let dateYear = date[2];
+  let dateMonth = date[1] * 10 / 10;
+  let dateDay = date[0] * 10 / 10;
+  let sortedPhoneList = phoneList.filter(e => {
+  let friendDate = (e.birthdate.split('.'));
+  let name = (e.name);
+  let friendYear = friendDate[2];
+  let friendMonth = friendDate[1] * 10 / 10;
+  let friendDays = friendDate[0] * 10 / 10;
+  if (friendYear <= dateYear) 
+    if (friendMonth > dateMonth)
+        return e;
+    if (friendMonth == dateMonth) 
+      if (friendDays > dateDay) 
+        return e;
+  })
+  // console.log(arr);
+  return sortedPhoneList.sort((a, b) => {
+    const friend1 = getDateObject(a.birthdate);
+    const friend2 = getDateObject(b.birthdate);
+
+    return friend1 - friend2;
+  })
 };
+
+function getDateObject(date){
+  date = date.split('.');
+  let firstDate = new Date();
+  firstDate.setFullYear(date[2], date[1] - 1 ,date[0]);
+  return firstDate;
+}
+
+const phoneList = [
+{
+  name: "Александра",
+  birthdate: "21.05.1978",
+},
+{
+  name: "Егор",
+  birthdate: "06.08.1976",
+},
+{
+  name: "Роман",
+  birthdate: "14.04.2000",
+},
+{
+  name: "Василий",
+  birthdate: "27.02.1950",
+},
+];;
+
+
+console.log(getNextBirthdays('25.02.1980', phoneList));
 
 /**
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
@@ -28,7 +90,7 @@
  *    friends: Array<Person>,
  *  }>}
  */
-export function getMonthsList(phoneList) {
+function getMonthsList(phoneList) {
 
 };
 
@@ -47,7 +109,7 @@ export function getMonthsList(phoneList) {
  *    totalPrice: number
  *  }}
  */
-export function getMinimumPresentsPrice(phoneList) {
+function getMinimumPresentsPrice(phoneList) {
 
 };
 
