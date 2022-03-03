2cb7dee9e40fd66bc92e087af282b5787f6b82ed
NikitaKirin <kirinnikita1406@gmail.com>
2022-03-03 12:12:58 +0500

    take first task

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const phoneList = [
+    {
+        name: "Виталий",
+        birthdate: "01.01.2001",
+    },
+    {
+        name: "Никита",
+        birthdate: "31.12.2001",
+    },
+    {
+        name: "Василиса",
+        birthdate: "15.10.2001",
+    },
+    {
+        name: "Александра",
+        birthdate: "21.05.2001",
+    },
+    {
+        name: "Егор",
+        birthdate: "06.08.1976",
+    },
+    {
+        name: "Роман",
+        birthdate: "14.04.2000",
+    },
+    {
+        name: "Василий",
+        birthdate: "27.02.1980",
+    },
+];
+
+const months = new Map([
+        ['00', 'январь'],
+        ['01', 'февраль'],
+        ['02', 'март'],
+        ['03', 'апрель'],
+        ['04', 'май'],
+        ['05', 'июнь'],
+        ['06', 'июль'],
+        ['07', 'август'],
+        ['08', 'сентябрь'],
+        ['09', 'октябрь'],
+        ['10', 'ноябрь'],
+        ['11', 'декабрь']
+    ]
+);
 /**
  * @typedef Person
  * @type {object}
@@ -17,9 +63,38 @@
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
  * @returns {Array<Person>} массив друзей, у которых дни рождения после даты отсчета
  */
- export function getNextBirthdays(date, phoneList) {
+function getNextBirthdays(date, phoneList) {
+    let resultArray = [];
+    date = getCorrectDate(date);
+    if (Array.isArray(phoneList) && typeof date == 'object') {
+        for (let i = 0; i < phoneList.length; i++) {
+            phoneList[i].birthdate = getCorrectDate(phoneList[i].birthdate);
+            if (phoneList[i].birthdate === false)
+                return [];
+            if (phoneList[i].birthdate.getFullYear() <= date.getFullYear()
+                && +phoneList[i].birthdate.getMonth() - +date.getMonth() > 0) {
+                resultArray.push(phoneList[i]);
+            }
+            if (phoneList[i].birthdate.getFullYear() <= date.getFullYear()
+                && +phoneList[i].birthdate.getMonth() - +date.getMonth() === 0
+                && +phoneList[i].birthdate.getDate() - +date.getDate() >= 0) {
+                resultArray.push(phoneList[i]);
+            }
+        }
+        resultArray.sort(function (a, b) {
+            if (a.birthdate.getMonth() > b.birthdate.getMonth())
+                return 1;
+            else if (a.birthdate.getMonth() === b.birthdate.getMonth())
+                return 0;
+            else
+                return -1;
+        })
 
-};
+        return resultArray;
+    }
+    return [];
+}
+;
 
 /**
  * @param {Array<Person>} phoneList - список друзей из телефонной книги
@@ -28,9 +103,9 @@
  *    friends: Array<Person>,
  *  }>}
  */
-export function getMonthsList(phoneList) {
-
-};
+function getMonthsList(phoneList) {
+}
+;
 
 /**
  * @param {Array<{
@@ -47,8 +122,28 @@ export function getMonthsList(phoneList) {
  *    totalPrice: number
  *  }}
  */
-export function getMinimumPresentsPrice(phoneList) {
+function getMinimumPresentsPrice(phoneList) {
 
 };
 
-module.exports = { getNextBirthdays, getMonthsList, getMinimumPresentsPrice };
+/**
+ * Get the Date object from correct string
+ * Преобразовать дату в корректный формат
+ */
+function getCorrectDate(date) {
+    if (typeof date != "string")
+        return false;
+    else {
+        date = date.split('.');
+        if (date[0].length === 2 && date[1].length === 2 && date[2].length === 4) {
+            return new Date(+date[2], +date[1]-1, +date[0]);
+        } else return false;
+    }
+}
+
+console.log(getNextBirthdays('01.01.2001', phoneList));
+// module.exports = {
+//     getNextBirthdays,
+//     getMonthsList,
+//     getMinimumPresentsPrice
+// };
