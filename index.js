5afa04a463682b4d2e4779f00eaf3e4875b8638a
equsix <Omar.m.zaid@hotmail.com>
2022-03-04 19:44:34 +0500

    fix

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const isValidDate = (dateString) => {
  const day = parseInt(parts[0], 10);
  const month = parseInt(parts[1], 10);
  const year = parseInt(parts[2], 10);
- if (month === 0 || month > 12) {
+ if (month == 0 || month > 12) {
   return false;
  }
 
