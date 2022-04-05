d5d5c4fff7bba3f19d74dccbf56e10f277547d21
Kirill <fatykovkirill@gmail.com>
2022-04-05 15:09:26 +0500

    попытка фикса

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
  * @returns {Array<Person>} массив друзей, у которых дни рождения после даты отсчета
  */
 export function getNextBirthdays(date, phoneList) {
-    if (typeof (date) !== "string") return false
+    if (typeof (date) !== "string") []
     const arraydate = date.split('.');
     const startday = +arraydate[0];
     const startmonth = +arraydate[1];
